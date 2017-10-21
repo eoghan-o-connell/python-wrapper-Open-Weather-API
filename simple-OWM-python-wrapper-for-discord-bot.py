f31e5579f981c5f7f2d970ad12de030613c91b32
Nadehh <32816721+Nadehh@users.noreply.github.com>
2017-10-21 22:50:06 +0100

    Changed functionality and structure
    changed the structure of time to update method and also removed some redundant code by changing the functionality of updateFields

--- simple-OWM-python-wrapper-for-discord-bot.py
+++ simple-OWM-python-wrapper-for-discord-bot.py
@@ -50,44 +50,38 @@ class WeatherUpdate(object):
         hoursBetweenLastUpdate = currenthour - self._time
         cityPrevAsked = location in self._citiesAsked
 
-        if self._location != location: #if the user has asked for a different city
-            if cityPrevAsked: #if we have the cities details previously stored in our citiesAsked dictionary
-                print("has been asked")
-                if currenthour - self._citiesAsked[location][1] < 3: #it is not time to update
-
-                    self.updateFields(location) #already have the fresh info for that city so no need to re send a request
-                else:
-                    self._time = currenthour #it is time to update so we update or time because that will be the time we most recently updated
-                    self.processFreshRequest(location)
-
-
-            else: #we know it hasnt been previously asked so now we update
-                print("we havent seen this before")
+        if self._location == location: #if the user has asked for the same city
+            if hoursBetweenLastUpdate >=3: #we have been asked the same location so we just need to check if it is time to update, if it isnt we do nothing
+                print("needs update")
                 self._time = currenthour
                 self.processFreshRequest(location)
 
-        elif hoursBetweenLastUpdate >=3: #we have been asked the same location so we just need to check if it is time to update, if it isnt we do nothing
-            print("needs update")
+        elif cityPrevAsked: #if we have the cities details previously stored in our citiesAsked dictionary but isnt our current city
+            print("has been asked")
+            if currenthour - self._citiesAsked[location][1] < 3: #it is not time to update
+
+                city = self._citiesAsked[location][0]
+                self.updateFields(city,location) #already have the fresh info for that city so no need to re send a request
+            else:
+                self._time = currenthour #it is time to update so we update or time because that will be the time we most recently updated
+                self.processFreshRequest(location)
+
+        else: #we know it hasnt been previously asked so now we update
+            print("we havent seen this before")
             self._time = currenthour
             self.processFreshRequest(location)
 
-        else: #we have been asked the same city and we dont need to re update as its within the time frame
-
-            print("this is already our current one so nothing needs to happen")
 
 
 
 
 
-    def updateFields(self,location):
+    def updateFields(self,dictionary,location):
 
         #if we already have the city stored and dont want to have to re send a request
-
-        city = self._citiesAsked[location][0]
-
-        self._mainWeather = city["weather"][0]["main"]
-        self._mainWeatherDesc = city["weather"][0]["description"]
-        self._temperature =  "%.2f" % (city["main"]["temp_max"] - 273.15)
+        self._mainWeather = dictionary["weather"][0]["main"]
+        self._mainWeatherDesc = dictionary["weather"][0]["description"]
+        self._temperature =  "%.2f" % (dictionary["main"]["temp_max"] - 273.15)
         self._location  = location
 
 
@@ -98,19 +92,13 @@ class WeatherUpdate(object):
 
         print("updating")
         request = requests.get('https://api.openweathermap.org/data/2.5/weather?q=%s&APPID=%s'% (location,self._apiKey))
-        dictionary = ast.literal_eval(request.text)
+        if request.status_code==200:
 
-        time = self._time
-        self._citiesAsked[location]=[dictionary,time]
+            dictionary = ast.literal_eval(request.text)
 
-
-
-        self._mainWeather = dictionary["weather"][0]["main"]
-        self._mainWeatherDesc = dictionary["weather"][0]["description"]
-        self._temperature =  "%.2f" % (dictionary["main"]["temp_max"] - 273.15)
-
-        self._location  = location
-
+            time = self._time
+            self._citiesAsked[location]=[dictionary,time]
+            self.updateFields(dictionary,location)
 
 if __name__ == '__main__':
 
